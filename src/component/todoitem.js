5946e7a28017b2b7136268514bde637e207721fa
Chuyun Huang <chuyunhuang@pursuit.org>
2019-02-26 17:11:37 -0500

    initial file setup with simple styling

---
+++ src/component/todoitem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './todoitem.css';
+
+const Todo =()=>{
+
+  return (
+    <div className="todo-list">
+      <div className="todo-item">
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+      </div>
+
+      <div className="todo-item">
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Todo
